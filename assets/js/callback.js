6c0e1d3dceecabe6a3e58fde4b558e2fdf4a5022
RyanAthaide <athaideryan1@gmail.com>
2026-01-08 12:11:00 -0300

    feat(tracking): implementar evento form_start e remover interaction.js, centralizando a lógica de eventos no mesmo arquivo

--- assets/js/callback.js
+++ assets/js/callback.js
@@ -1,6 +1,26 @@
   const form = document.getElementById('contact-form');
   const feedback = document.getElementById('form-feedback')
 
+// Captura de interacao ao formulario
+  let startForm = false;
+
+  form.addEventListener('input', () => {
+    if (!startForm) {
+      startForm = true;
+
+      window.dataLayer.push({
+        event: 'form_start',
+        custom_section: 'contact',
+        custom_type: 'lead',
+        custom_title: 'start_fill'
+      });
+
+      console.log("form_start enviado!")
+    }
+  });
+
+
+// Captura do formulario enviado
   form.addEventListener('submit', function (event) {
     event.preventDefault(); // evita reload da página
 
@@ -9,11 +29,13 @@
       event: 'callback',
       custom_section: 'contact',
       custom_type: 'lead',
-      custom_title: 'contact_me',
+      custom_title: 'contact_me'
     });
 
     console.log('Evento enviado para o dataLayer');
 
+
+    
      // Mostrar feedback
   feedback.classList.remove('hidden');
   feedback.classList.add('show');
@@ -28,3 +50,4 @@
   form.reset();
 
   });
+
